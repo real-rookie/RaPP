7e66911afed1547817b2e907129ea430df4e1986
zl <zhiwen5@ualberta.ca>
2023-08-22 16:50:27 -0600

    requirements for ubuntu

--- src/train.py
+++ src/train.py
@@ -97,7 +97,7 @@ if __name__ == "__main__":
     parser.add_argument("--model", type=str, default="ae", choices=["ae", "aae", "vae"])
     parser.add_argument("--dataset", type=str, default="mnist")
     parser.add_argument("--target_label", type=int, default=0)
-    parser.add_argument("--data_dir", type=str, default="/")
+    parser.add_argument("--data_dir", type=str, default="./")
     parser.add_argument("--hidden_size", type=int, default=20)
     parser.add_argument("--n_layers", type=int, default=3)
     parser.add_argument("--max_epochs", type=int, default=200)
